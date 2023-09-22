ac32f731567385438ec47380aa7d02233f8aa850
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-22 16:38:46 +0200

    .

--- mymap.js
+++ mymap.js
@@ -3,7 +3,7 @@ class myMap extends Map {
         if (this.has(p)) {
             var l = super.get(p)
             if (typeof l == 'number' && typeof r == 'number')
-                super.set(p, (r + l) / 2)
+                super.set(p, r * l)
             else if(typeof l == 'string' && typeof r == 'string')
                 super.set(p, l + r)
             else
--- mymap.test.js
+++ mymap.test.js
@@ -1,10 +1,10 @@
 const myMap = require('./mymap')
-test('average test', () => {
+test('number_prod test', () => {
     s = new myMap()
     s.set(2, 4)
     s.set(3, 7)
     s.set(2, 6)
-    expect(s.get(2)).toBe(5)
+    expect(s.get(2)).toBe(24)
 })
 test('string test', () => {
     s = new myMap()
